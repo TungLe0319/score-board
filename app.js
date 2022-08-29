582577c816dfb4e7e0c31771fd2d25ee7259c37f
Tung Le <tung.le0319@gmail.com>
2022-08-29 09:25:40 -0600

    testing

--- app.js
+++ app.js
@@ -16,8 +16,27 @@ function incrementClick() {
   console.log(RandomNumber());
 }
 
+
+
+
+function risky(){
+  Score.innerText = teamScore;
+  ScoreTwo.innerText = enemyScore
+if (RandomNumber() >5) {
+  teamScore+=3
+}else{
+  enemyScore+=3
+}
+
+}
+
 //NOTE  HAVE TO RETURN THE VALUE USING return()
 //NOTE The Math.floor() function returns the largest integer less than or equal to a given number.
 function RandomNumber() {
   return Math.floor(Math.random() * 10);
+}
+
+
+function save(){
+  window.localStorage.setItem(Score)
 }
